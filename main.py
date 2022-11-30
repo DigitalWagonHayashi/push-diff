7c7ba1bc6ff46a4fc8b9e8c3f4661e4f623d3559
haruki hayashi <haruki@harukinoMacBook-Pro.local>
2022-11-30 15:02:26 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from numpy import uint8
+
+
+def push_diff():
+    img1_path = '/Users/haruki/img1.png'
+    img2_path = '/Users/haruki/img1.png'
+    img1 = cv2.imread(img1_path, -1)
+    img2 = cv2.imread(img2_path, -1)
+    print(type(img2))
+    print(type(img2[0]))
+    print(type(img2[0][0]))
+    print(img2[0][0].dtype)
+    diff_coordinate = []
+    for height in range(len(img1)):
+        diff_coordinate_tmp = []
+        for width in range(len(img1[0])):
+            if list(img1[height][width]) != list(img2[height][width]):
+                diff_coordinate_tmp.append(img2[height][width])
+            else:
+                img2[height][width][3] = 0
+                diff_coordinate_tmp.append(img2[height][width])
+        diff_coordinate.append(diff_coordinate_tmp)
+    print(diff_coordinate[0])
+    cv2.imwrite('/Users/haruki/imgimg.png', np.array(diff_coordinate))
+
+
+if __name__ == '__main__':
+    push_diff()
+    print()
